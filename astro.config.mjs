2532972ee829fc4d6e8fed96e726055a2dd7ddf8
milton <milton.olivares0999@gmail.com>
2026-01-23 18:44:35 -0600

    Fix Pages Functions: Use static output with functions directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  output: 'server',
+  output: 'static',
   vite: {
     optimizeDeps: {
       exclude: ['decap-cms']
